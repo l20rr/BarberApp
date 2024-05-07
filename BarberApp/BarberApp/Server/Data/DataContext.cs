6d6dd4042b703f510cde1a20eec0f56e88ee364d
Lucas Rocha <l.rochasantos20@gmail.com>
2024-05-07 12:59:50 +0100

    Db connection and user created

---
+++ BarberApp/BarberApp/Server/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using BarberApp.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BarberApp.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BarberApp/BarberApp/Shared/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberApp.Shared
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Digite o Email!")]
+        public required string UserEmail { get; set; }
+        [Required(ErrorMessage = "Digite o Nome!")]
+        public required string UserName { get; set; }
+
+        public int UserPhone { get; set; }
+
+        public string? UserAdress { get; set; }
+        [Required(ErrorMessage = "Digite o Nome da sua loja!")]
+        public required string ShopName { get; set; }
+        [Required(ErrorMessage = "Digite a senha!")]
+        public required string UserPassword { get; set; }
+    }
+}
